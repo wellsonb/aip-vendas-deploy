4e7e77980a505e7ee8e2da25c4cebbd5a7875231
Wellson Bandeira <wellsonb@gmail.com>
2022-11-09 17:45:40 -0300

    Production configuration

---
+++ src/modules/users/routes/sessions.routes.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response, Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+import SessionsController from '../controllers/SessionsController';
+
+const sessionsRouter = Router();
+const sessionsController = new SessionsController();
+
+sessionsRouter.post(
+  '/',
+  (request: Request, response: Response, next: NextFunction) => {
+    // console.log(request.body);
+    next();
+  },
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  sessionsController.create,
+);
+export default sessionsRouter;
